370157936edd810e233c124c875f4d7dc6125f0b
Michael Rooney <mrooney.github@rowk.com>
2012-09-15 15:49:12 -0400

    fix clicking bug

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ $(function() {
         $('.controls')
             .css({'backgroundColor': '#5bb75b'})
             .animate({'backgroundColor': '#fff'}, 3000);
-        $(this).empty();
+        $(this).empty().unbind();
         setTimeout(game.init, 3000);
     }).click();
 });
