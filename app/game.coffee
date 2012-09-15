058bd193434ba54b0e1f9e561b94f030c5aa60f7
David Mauro <dave@canv.as>
2012-09-15 15:40:56 -0400

    Multiple players joining a lobby works now.

--- app/game.coffee
+++ app/game.coffee
@@ -10,6 +10,7 @@ class Lobby
     constructor: (@id) ->
         @players = []
         @ready = false
+        @full = false
         _lobbies[@id] = @
 
     broadcast: (name, data) ->
@@ -21,17 +22,39 @@ class Lobby
             player.listen_for_start()
 
     add_player: (player) ->
+        @broadcast 'player_added'
         @players.push player
         player.lobby = @
-        @ready = @players.length >= _min_players
-        player.socket.emit 'added_to_lobby', { id : @id }
-        if @ready
+        @full = @players.length >= _max_players
+        player.socket.emit 'added_to_lobby', {
+            id              : @id
+            is_ready        : @ready
+            player_count    : @players.length
+        }
+        if @players.length >= _min_players and not @ready
+            @ready = true
             console.log "listening for start and broadcasting ready"
             @listen_for_start()
             @broadcast 'ready'
+        else if @ready
+            player.listen_for_start()
+
+    remove_player: (player) ->
+        @broadcast 'player_dropped', {
+            id : player.id
+        }
+        for i in [0...@players.length]
+            if @players[i] is player
+                @players.splice i, 1
+                break
+        if @ready and @players.length < _min_players
+            @ready = false
+            @broadcast 'unready'
+        @full = @players.length >= _max_players
 
     start_game: ->
-        @broadcast 'start_game'
+        for player in @players
+            player.start_game()
 
 class Player
     constructor: (@socket, @id = new Date().getTime()) ->
@@ -40,6 +63,8 @@ class Player
         # Listen for events
         @socket.on 'lobby', (data) =>
             @put_in_lobby data
+        @socket.on 'disconnect', (data) =>
+            @lobby.remove_player @
         @socket.emit 'connection_established'
 
     put_in_lobby: (data) ->
@@ -51,7 +76,7 @@ class Player
         unless lobby
             # Try to find a lobby for them
             for _, _lobby of _lobbies
-                if not _lobby.ready
+                if not _lobby.full
                     lobby = _lobby
                     break
         unless lobby
@@ -61,8 +86,19 @@ class Player
         lobby.add_player @
 
     listen_for_start: ->
+        console.log "Listening for this player"
         @socket.on 'start_game', =>
+            console.log "WE GOT THE CLICK"
             @lobby.start_game()
+
+    start_game: ->
+        other_players = []
+        for player in @lobby.players
+            continue if player is @
+            other_players.push player.id
+        @socket.emit "start_game", {
+            other_players   : other_players
+        }
 
 module.exports.init = (io) ->
     io.sockets.on 'connection', (socket) ->
--- static/js/game/networking.js
+++ static/js/game/networking.js
@@ -1,26 +1,72 @@
 (function() {
+    var _player_count = 0;
+
+    _listen_for_other_players = function() {
+        socket.on("player_move", function(data) {
+            var player = game.other_players[data.id];
+            // Move to data.x, data.y
+        });
+        socket.on("player_steal", function(data) {
+            var player = game.other_players[data.id];
+            // This player is trying to steal from data.target
+        });
+    }
+
+    _ready_for_start = function() {
+        console.log("WE HAVE ENOUGH PLAYERS");
+        // We can press start to begin now, or another player can
+        window.button = $('<div style="width:300px;height:50px;background:blue;margin:20px auto;cursor:poiner;">Start</div>');
+        $('body').append(button);
+        button.click(function() {
+            console.log("click");
+            socket.emit("start_game");
+        })
+        socket.on("start_game", function(data) {
+            game.other_players = data.other_players
+            game.begin();
+            button.remove();
+            _listen_for_other_players();
+        });
+    }
+
     window.socket_connect = function() {
         window.socket = io.connect('http://127.0.0.1:1428');
         socket.on("connection_established", function() {
-            data = (game.seed) ? { id: game.seed } : null;
+            var data = (game.seed) ? { id: game.seed } : null;
             socket.emit("lobby", data);
             socket.on("added_to_lobby", function(data) {
-                console.log("WE ARE IN A LOBBY");
+                _player_count = data.player_count;
+                console.log("WE ARE IN A LOBBY", data.id, " WITH X PLAYERS", data.player_count);
+                if (data.is_ready) {
+                    console.log("Joined a game already ready");
+                    _ready_for_start();
+                }
                 game.random = alea_random(data.id);
+                socket.on("player_added", function() {
+                    console.log("A NEW PLAYER JOINED YOUR LOBBY");
+                    _player_count++;
+                });
+                socket.on("player_dropped", function(data) {
+                    console.log("A PLAYER HAS LEFT YOUR LOBBY");
+                    _player_count--;
+                });
                 socket.on("ready", function() {
-                    console.log("WE HAVE ENOUGH PLAYERS");
-                    // We can press start to begin now, or another player can
-                    var button = $('<div style="width:300px;height:50px;background:blue;margin:20px auto;">Start</div>');
-                    $('body').append(button);
-                    button.click(function() {
-                        socket.emit("start_game");
-                    })
-                    socket.on("start_game", function() {
-                        game.begin();
-                        button.remove();
-                    });
+                    _ready_for_start();
                 });
             });
         });
     }
+
+    window.player_move = function(x, y) {
+        socket.emit("player_move", {
+            x : x,
+            y : y
+        });
+    };
+
+    window.player_steal = function(target) {
+        socket.emit("player_steal", {
+            target : target
+        });
+    };
 })();
